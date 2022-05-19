82f7f396cf4446e1cd981bf0215c526599261c57
ashot-developer <markosyans.ashot@gmail.com>
2022-05-19 12:32:21 +0400

    Small change

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -2,13 +2,19 @@ class ColorPicker {
   constructor(params) {
     this.dom = params.dom;
     this.data = params.data;
-    this.listener = params.listener;
+    let { listeners } = params;
+    this.listeners = listeners;
+    this.handleClick = this.handleClick.bind(this);
 
     this.init();
   }
 
+  handleClick() {
+    return this.listeners.onClick && this.listeners.onClick(this.$el, this.data);
+  }
+
   bindEvents() {
-    this.dom.addEventListener('click', this.listener);
+    this.dom.onclick = this.handleClick;
   }
 
   bindData() {
